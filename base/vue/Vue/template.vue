c275e42376de7425cd5c58a43dfc4721dadc5c6f
vuact <74364990+Vuact@users.noreply.github.com>
2021-05-31 16:16:06 +0800

    Update template.vue

--- base/vue/Vue/template.vue
+++ base/vue/Vue/template.vue
@@ -1,1 +1,50 @@
 
+```js
+<template>
+  <div></div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions, mapMutations } from "vuex";
+
+export default {
+  name: "template name",
+  inheritAttrs: false,
+  props: {
+    my_txt: {
+      type: String,
+      default: "",
+    },
+    my_Arr: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  filters() {},
+  computed() {
+	...mapState({}),
+	...mapGetters({})
+  },
+  methods() {
+	...mapActions({}),
+	...mapMutations({}),
+  },
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
+};
+</script>
+```
